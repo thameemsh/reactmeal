ef9e0dfe441016c3c66ee08c662055acc5d3ea64
Mohamed Thameem <thameemsh@gmail.com>
2023-01-16 22:33:14 -0500

    Add Meals and child(AvailableMeals and MealsSummary) components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import AvailableMeals from "./AvailableMeals"
+import { Fragment } from "react"
+import MealsSummary from "./MealsSummary"
+const Meals = () => {
+ return <Fragment>
+    <MealsSummary />
+    <AvailableMeals />
+ </Fragment>
+}
+
+export default Meals
