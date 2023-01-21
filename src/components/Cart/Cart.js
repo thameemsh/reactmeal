c7a0b1b8d5e61a21a6b646fb7092df856bd41520
Mohamed Thameem <thameemsh@gmail.com>
2023-01-21 01:29:38 -0500

    Add Modal state and manage Cart opening/closing

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
-import classes from "./Card.module.css";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 
-const Card = (props) => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -9,18 +10,18 @@ const Card = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>39.56</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
-export default Card;
+export default Cart;
