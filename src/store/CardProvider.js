fd6f28cac4778f0aea1fabfdc1055c59c1b73d66
Mohamed Thameem <thameemsh@gmail.com>
2023-01-21 02:39:54 -0500

    Modify CartProvider Component to dynamically update cart items and amount

--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -1,21 +1,51 @@
 import CartContext from "./card-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item); //concat is similar to push except it doesn't mutate the original array
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 const CartProvider = (props) => {
-    const addItemToCartHandler = item => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD",
+      item: item,
+    });
+  };
 
-    }
-
-    const removeItemFromCartHandler = id => {
-
-    }
-    const cartContext = {
-        items:[],
-        totalAmount:0,
-        addItem:  addItemToCartHandler ,
-        removeItem: removeItemFromCartHandler
-    }
-    return <CartContext.Provider value={cartContext}>
-        {props.children}
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({
+      type: "REMOVE",
+      id: id,
+    });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
     </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
